f774b636981d3b6af31a31ab2640cdac119ccb80
AditRobertho <aditjeneri@gmail.com>
2024-07-29 06:57:18 +0700

    # create LittleLemon database # create model for booking and menu

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    booking_id = models.IntegerField(primary_key=True, unique=True, null=False)
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+class Menu(models.Model):
+    menu_id = models.IntegerField(primary_key=True, unique=True, null=False)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
